f587d8d00ef3788f1f8905824141624710f266b4
unknown <20111224i705@20111224i705-PC.(none)>
2013-04-23 15:08:02 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- b2cservice/index.php
+++ b2cservice/index.php
@@ -6,18 +6,13 @@ define('ET_ROOT', dirname(__FILE__));
 define('MODE_NAME','mycore');
 define('THINK_PATH',dirname(__FILE__).'/../../ThinkPHP/');
 define('APP_NAME', 'b2cservice');
-define('APP_PATH', './B2Cservice/');
+define('APP_PATH', './B2CService/');
 define('DEFAULT_TYPE','default');
 define('__PUBLIC__',ET_URL."Public");
 define('SITE_URL',ET_URL);
 define('APP_DEBUG', true);
-var_dump(THINK_PATH);
-var_dump(APP_PATH);
-exit;
 require(APP_PATH.'Common/Function.php');
 require(APP_PATH.'Common/B2bFunction.php');
-var_dump(THINK_PATH);
-//require(THINK_PATH.'ThinkPHP.php');
-var_dump(THINK_PATH);
+require(THINK_PATH.'ThinkPHP.php');
 
 ?>
