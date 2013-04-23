8a7f5784b0bf1a9eda970c5c41d5d49ccff1f628
toamture <87068928@qq.com>
2013-04-23 15:23:21 +0800

    Signed-off-by: toamture <87068928@qq.com>

--- b2cservice/apis/define.inc.js
+++ b2cservice/apis/define.inc.js
@@ -1,12 +1,12 @@
 
 
 	//服务器地址
-	server_url = 'http://www.myerpcenter.com';
+	server_url = 'http://c.dlgulian.com';
 	//获得服务器数据
     server_xianlu_url = server_url+"/index.php?s=/Server/getxianlubyID";
 	
 	//服务器地址
-	site_url = 'http://www.testgulianlvyou.com';
+	site_url = 'http://www.lvyou.com';
 	//用户登录信息验证地址
     loginsta_url = site_url+"/b2cservice/apis/ajax_loginsta.php";
 	//订单流程填写订单地址
