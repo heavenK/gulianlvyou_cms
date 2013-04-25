d729e3781b92a2e8a9480e056f5657da524cd815
unknown <20111224i705@20111224i705-PC.(none)>
2013-04-25 16:25:32 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- b2cservice/apis/define.inc - 副本.js
+++
@@ -1,5 +0,0 @@
-
-	//本地
-	site_url = 'http://www.gulianlvyou.com/';
-	//服务器
-	server_url = 'http://c.dlgulian.com/';
--- b2cservice/define.inc - 副本.php
+++
@@ -1,6 +0,0 @@
-<?php
-	//本地
-    define('SITE_URL','http://www.gulianlvyou.com/');
-	//服务器
-    define('SERVER_URL','http://c.dlgulian.com/');
-?>
