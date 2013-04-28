ec5991bdc7abbdc41a1e37338bf3df52ee0bc8e9
heavenK <heaven@we54.com>
2013-04-28 10:46:18 +0800

    Update define2.inc.js

--- b2cservice/apis/define2.inc.js
+++ b2cservice/apis/define2.inc.js
@@ -1,4 +1,3 @@
-
 	//获得服务器数据
     server_xianlu_url = server_url+"index.php?s=/Server/getxianlubyID";
 	//本地服务地址
@@ -7,3 +6,4 @@
     loginsta_url = site_service_url+"DEDEInfo/ajax_loginsta";
 	//订单流程填写订单地址
     dingdan_1_url = site_url+"b2cservice/index.php?s=/Order/book1";
+
