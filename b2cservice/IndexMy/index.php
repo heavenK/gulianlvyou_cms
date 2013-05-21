c2e517ee013c0f6beab30558b783b9f1655b7d3f
unknown <20111224i705@20111224i705-PC.(none)>
2013-05-21 16:20:11 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- b2cservice/IndexMy/index.php
+++ b2cservice/IndexMy/index.php
@@ -5,7 +5,6 @@ define('SITE_INDEX',MY_URL.'index.php?s=/');
 define('SITE_URL',MY_URL);
 define('__T_PUBLIC__',MY_URL.'Public');
 define('DEFAULT_ACTION', 'My');
-define('APP_DEBUG', true);		// Use debug 
 
 require(THINK_PATH.'ThinkPHP.php');
 
--- b2cservice/index.php
+++ b2cservice/index.php
@@ -5,7 +5,6 @@ define('SITE_INDEX',B2CSERVICE_URL.'index.php?s=/');
 define('SITE_URL',B2CSERVICE_URL);
 define('__T_PUBLIC__',MY_URL.'Public');
 define('DEFAULT_ACTION', 'My');
-define('APP_DEBUG', true);		// Use debug 
 
 require(THINK_PATH.'ThinkPHP.php');
 	
