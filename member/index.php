92739647f7c945cf30e53f7644938149a6a2c7eb
unknown <20111224i705@20111224i705-PC.(none)>
2013-05-21 17:15:00 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- member/index.php
+++ member/index.php
@@ -10,7 +10,7 @@ require_once(dirname(__FILE__)."/config.php");
 //跳转
 require_once(dirname(__FILE__)."/../b2cservice/define2.inc.php");
 if($cfg_ml->IsLogin()){
-	include(MY_URL);
+	//include(MY_URL);
 }else{
 	include(APP_PATH."Tpl/Index/login.html");
 }
