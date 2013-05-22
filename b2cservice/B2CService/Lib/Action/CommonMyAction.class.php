cc6b2f5175d3dfaf08cd6251b09448aa9d836d6c
unknown <20111224i705@20111224i705-PC.(none)>
2013-05-22 16:32:58 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- b2cservice/B2CService/Lib/Action/CommonMyAction.class.php
+++ b2cservice/B2CService/Lib/Action/CommonMyAction.class.php
@@ -7,10 +7,13 @@ class CommonMyAction extends Action{
     public function _initialize() {
         $M_ID = GetNum(GetCookie("DedeUserID"));
 		$cfg_cookie_encode = GetDEDEInfo('cfg_cookie_encode');
+		echo "<pre>";
+		dump($_COOKIE);
+		
 		//用户未登录
 		$this->loginsta = A("DEDEInfo")->ajax_loginsta('arrary');
-        if (false === $this->loginsta)
-            redirect(ROOT_URL.'member');
+//        if (false === $this->loginsta)
+//            redirect(ROOT_URL.'member');
 		$this->_myinit();	
     }
 	
