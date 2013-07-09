17b9e3d780c6e06ab6a0d9661682626ac6e8ba09
unknown <Kaiser@Kaiser-PC.(none)>
2013-07-09 14:24:29 +0800

    Signed-off-by: unknown <Kaiser@Kaiser-PC.(none)>

---
+++ plus/faq_about.php
@@ -0,0 +1,6 @@
+<?php
+require_once(dirname(__FILE__)."/../include/common.inc.php");
+
+echo "document.write('<ol><li><b><img src=\"/demand/images/pic_01.jpg\" width=\"16\" height=\"16\" /></b><h1><a href=\"#\" target=\"_blank\">丢丢</a>:&nbsp;夏日悠游漓江山夏日悠游漓江山夏日悠游漓江山</h1></li><li><b><img src=\"/demand/images/pic_01.jpg\" width=\"16\" height=\"16\" /></b><h1><a href=\"#\" target=\"_blank\">丢丢</a>:&nbsp;夏日悠游漓江山</h1></li><li><b><img src=\"/demand/images/pic_01.jpg\" width=\"16\" height=\"16\" /></b><h1><a href=\"#\" target=\"_blank\">丢丢</a>:&nbsp;夏日悠游漓江山</h1></li></ol>');";
+
+?>
