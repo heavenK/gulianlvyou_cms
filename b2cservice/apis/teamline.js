b0555b6ce41785c15d9181e97d5022b8283985a5
unknown <20111224i705@20111224i705-PC.(none)>
2013-07-11 12:23:49 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- b2cservice/apis/teamline.js
+++ b2cservice/apis/teamline.js
@@ -92,17 +92,16 @@ function initFloatPerson(group) {
     $("#Popupbox ul").remove();
     var text = "";
 	text += '<ul>'
-		+ '<li class="pwidth">标准零售价：</li><li class="pwidth2"><b class="piaohong">￥' + group.adult_price + '</b></li>';
+		+ '<li class="pwidth">标准零售价：</li><li class="pwidth2"><b class="piaohong">￥' + group.adult_price + '</b></li>'
 		+ '<li> <span>成人：</span> <span class="add_count"><input type="text" value="1"  /><span class="less"></span> <span class="add"></span> </span> <span>&nbsp;人</span> </li>'
-		+ '<li> <span>成人：</span> <span class="add_count"><input type="text" value="1"  /><span class="less"></span> <span class="add"></span> </span> <span>&nbsp;人</span> </li>'
-//		+ '<li> <span>儿童：</span> <span class="add_count"><input type="text" value="0"  /><span class="less"></span> <span class="add"></span> </span> <span>&nbsp;人</span> </li>'
-		+ '</ul>';
-		  
+		+ '<li> <span>儿童：</span> <span class="add_count"><input type="text" value="0"  /><span class="less"></span> <span class="add"></span> </span> <span>&nbsp;人</span> </li>'
+		+'</ul>';
+		
 		
     $("#order_select").html(text);
     //$(text).insertAfter(".pro_table");
 	
-	
+/*	
     $("#Popupbox ul input").bind("keyup", function() { this.value = this.value.replace(/\D/g, ''); if (this.value == "") this.value = "0"; computeprice(this, 1); });
     $("#Popupbox ul input").bind("afterpaste", function() { this.value = this.value.replace(/\D/g, ''); if (this.value == "") this.value = "0"; computeprice(this, 1); });
     $("#Popupbox ul span .add").bind("click", function() { var inp = $(this).parent().find("input").eq(0); inp.val(parseInt(inp.val()) + 1); computeprice(this, 2); });
@@ -130,6 +129,10 @@ function initFloatPerson(group) {
     $(".pro_table th div").hover(function() { $(this).show(); }, function() { $(this).hide(); })
 
     computeprice();
+	
+*/	
+	
+	
 }
 
 
