e56067b6171f40fb0c577dcdc84ae5d54fe0a81d
unknown <20111224i705@20111224i705-PC.(none)>
2013-07-11 09:33:04 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- b2cservice/apis/teamline.js
+++ b2cservice/apis/teamline.js
@@ -94,6 +94,9 @@ function initFloatPerson(group) {
 	text += '<ul><li class="pwidth">标准零售价：</li><li class="pwidth2"><b class="piaohong">￥' + group.adult_price + '</b></li>'
 		+ '<li> <span>成人：</span> <span class="add_count"><input type="text" value="1"  /><span class="less"></span> <span class="add"></span> </span> <span>&nbsp;人</span> </li>'
 		+ '<li> <span>儿童：</span> <span class="add_count"><input type="text" value="0"  /><span class="less"></span> <span class="add"></span> </span> <span>&nbsp;人</span> </li></ul>';
+		
+	alert(text)	
+		
     $(text).insertAfter(".pro_table");
     $("#Popupbox ul input").bind("keyup", function() { this.value = this.value.replace(/\D/g, ''); if (this.value == "") this.value = "0"; computeprice(this, 1); });
     $("#Popupbox ul input").bind("afterpaste", function() { this.value = this.value.replace(/\D/g, ''); if (this.value == "") this.value = "0"; computeprice(this, 1); });
