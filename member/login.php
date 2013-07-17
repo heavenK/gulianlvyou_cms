52cc82b1407116cb212132f9517b113d0ae28e22
unknown <20111224i705@20111224i705-PC.(none)>
2013-07-17 16:50:46 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

--- member/login.php
+++ member/login.php
@@ -7,9 +7,17 @@
  * @link           http://www.dedecms.com
  */
 require_once(dirname(__FILE__)."/config.php");
-if($cfg_ml->IsLogin())
-{
-    ShowMsg('你已经登陆系统，无需重新注册！', 'index.php');
-    exit();
+require_once(dirname(__FILE__)."/../b2cservice/apis/define3.inc.php");
+//if($cfg_ml->IsLogin())
+//{
+//    ShowMsg('你已经登陆系统，无需重新注册！', 'index.php');
+//    exit();
+//}
+//require_once(dirname(__FILE__)."/templets/login.htm");
+if($cfg_ml->IsLogin()){
+	//跳转
+	my_redirect(MY_URL);
+}else{
+	include(APP_PATH."Tpl/My/login.html");
 }
-require_once(dirname(__FILE__)."/templets/login.htm");
+exit;
